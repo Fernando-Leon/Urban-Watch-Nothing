3de8e262eb49cfb4d2bdc146812b81e08878e774
Fernando-Leon <leon.monroy.fernando@gmaiol.com>
2023-06-28 11:44:13 -0600

    first commit

---
+++ js/transition.js
@@ -0,0 +1,35 @@
+const setBackgroundNav = (id, idSection, backgroundColorNav, opacityLevel = "1", time) => {
+    $(id).on('click', function(e) {
+        e.preventDefault();
+        $("html, body").animate({scrollTop: $(idSection).offset().top }, time);
+        setTimeout(()=>{
+            document.getElementById("cabeza").style.backgroundColor = backgroundColorNav;
+            document.getElementById("cabeza").style.opacity = opacityLevel;
+            // Guardar los valores en el almacenamiento local
+        },time);
+    });
+}
+
+
+$(document).ready(function() {
+    setBackgroundNav('#a1', '#inicio', '#2C302F', '.9', 1000);
+    setBackgroundNav('#a2', '#caracteristicas', '#2C302F', '.9', 1000);
+    setBackgroundNav('#a3', '#beneficios', '#2C302F', '.9', 1000);
+    setBackgroundNav('#a4', '#descargar', '#2C302F', '.9', 1000);
+    setBackgroundNav('#a5', '#contacto', '#2C302F', '.2', 1000);
+    setBackgroundNav('#a-return', '#main', 'transparent', '1', 1000);
+});
+
+$(window).on("beforeunload", function(event) {
+    //event.preventDefault(); // Prevenir que el navegador muestre el mensaje de confirmación predeterminado
+      $("html, body").animate({scrollTop: $('#main').offset().top }, time);
+      $("#cabeza").css("backgroundColor", "transparent");
+      $("#cabeza").css("opacity", "1");
+      location.reload();
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.key === "Tab") {
+        event.preventDefault(); // Prevenir el comportamiento predeterminado de la tecla Tab
+    }
+});
